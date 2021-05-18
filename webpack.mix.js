d1a3513b653f4a02d38786647001c85a8515dd48
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-18 11:24:41 -0700

    Utilizes Laravel Mix's file hashing method mix.version() for cache-busting purposes - Updates welcome.blade file to reference the mix helper function

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css')
-    .browserSync('127.0.0.1:3000/');
+    .browserSync('127.0.0.1:3000/')
+    .version();
